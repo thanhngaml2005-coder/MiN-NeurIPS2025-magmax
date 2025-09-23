75ad0ea8fe59aea4987ee9113974d52f5549cf96
ASCIIJK <150874756+ASCIIJK@users.noreply.github.com>
2025-09-23 16:12:06 +0800

    Update run.sh

--- MiN/run.sh
+++ MiN/run.sh
@@ -3,3 +3,7 @@ python main.py --base_configs configs/base_configs/cifar224.json --model_configs
 python main.py --base_configs configs/base_configs/cub.json --model_configs configs/model_configs/MiN-cub-10steps.json
 
 python main.py --base_configs configs/base_configs/imagenetr.json --model_configs configs/model_configs/MiN-inr-10steps.json
+
+python main.py --base_configs configs/base_configs/food101.json --model_configs configs/model_configs/MiN-food-10steps.json
+
+python main.py --base_configs configs/base_configs/omnibenchmark.json --model_configs configs/model_configs/MiN-omni-10steps.json
